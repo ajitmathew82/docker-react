4eb2013dfa089a59ba54d9a18e768c7d0883dbfd
Ajit Mathew <ajitmathew82@yahoo.co.in>
2022-02-26 19:09:35 +0000

    Updated Travis.yml v2 file with second user

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was changed on a V2 of the Feature branch</p>
+        <p>I was changed on a V3wq! of the Feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
